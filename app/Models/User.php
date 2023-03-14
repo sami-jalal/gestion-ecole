ff8563a121422f0b5fc0347d631483abf01ad570
sami-jalal <jsami.jalal.sami@gmail.com>
2023-03-14 19:14:26 +0100

    v1.3 ajout de breeze et systéme de rôoôle

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function users() {
+        $this->belongsToMany(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,36 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth', 'role:admin'])->group((function () {
-    Route::get('/users', function () {
-        return 'users Hello Admin';
-    });
-    
-    Route::get('/teachers', function () {
-        return 'teachers Admin';
-    });
-    
-    Route::get('/students', function () {
-        return 'students Admin';
-    });
-    
-    Route::get('/classes', function () {
-        return 'classes Admin';
-    });
-}));
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', 'role:teacher'])->group((function () {
-    
-    Route::get('/teachers', function () {
-        return 'teachers Admin';
-    });
-    
-    Route::get('/students', function () {
-        return 'students Admin';
-    });
-    
-    Route::get('/classes', function () {
-        return 'classes Admin';
-    });
-    
-}));
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
