ee925a07633ebc7150a424461873eec3a5428f4b
sami-jalal <jsami.jalal.sami@gmail.com>
2023-04-07 14:49:42 +0000

    Modification de l'affichage du tableau de bord selon le profile

--- app/Services/ServiceDash.php
+++ app/Services/ServiceDash.php
@@ -14,36 +14,49 @@ class ServiceDash
         $nbr_students = User::all()->where('role', '=', 'student')->count();
         $nbr_courses = Course::all()->count();
         
-        $data = [
-            [
-                'title' => 'Administrateurs',
-                'count' => $nbr_admin,
-                'icon' => 'fas fa-users-cog',
-                'bg_color' => '#17a2b8',
-                'route' => 'users.get_all'
-            ],
-            [
-                'title' => 'Enseignants',
-                'count' => $nbr_teachers,
-                'icon' => 'fas fa-chalkboard-teacher',
-                'bg_color' => '#00a857',
-                'route' => 'users.get_all'
-            ],
-            [
-                'title' => 'Etudiants',
-                'count' => $nbr_students,
-                'icon' => 'fas fa-user-graduate',
-                'bg_color' => '#f58742',
-                'route' => 'users.get_all'
-            ],
-            [
-                'title' => 'Cours',
-                'count' => $nbr_courses,
-                'icon' => 'fas fa-book',
-                'bg_color' => '#aec724',
-                'route' => 'courses.get_all'
-            ]
-        ];
+        if(auth()->user()->role === "admin") {
+            $data = [
+                [
+                    'title' => 'Administrateurs',
+                    'count' => $nbr_admin,
+                    'icon' => 'fas fa-users-cog',
+                    'bg_color' => '#17a2b8',
+                    'route' => 'users.get_all'
+                ],
+                [
+                    'title' => 'Enseignants',
+                    'count' => $nbr_teachers,
+                    'icon' => 'fas fa-chalkboard-teacher',
+                    'bg_color' => '#00a857',
+                    'route' => 'users.get_all'
+                ],
+                [
+                    'title' => 'Etudiants',
+                    'count' => $nbr_students,
+                    'icon' => 'fas fa-user-graduate',
+                    'bg_color' => '#f58742',
+                    'route' => 'users.get_all'
+                ],
+                [
+                    'title' => 'Cours',
+                    'count' => $nbr_courses,
+                    'icon' => 'fas fa-book',
+                    'bg_color' => '#aec724',
+                    'route' => 'courses.get_all'
+                ]
+            ];
+        } elseif (auth()->user()->role === "teacher") {
+            $data = [
+                [
+                    'title' => 'Cours',
+                    'count' => $nbr_courses,
+                    'icon' => 'fas fa-book',
+                    'bg_color' => '#aec724',
+                    'route' => 'courses.get_all'
+                ]
+            ];
+        }
+        
         
         return $data;
     }
